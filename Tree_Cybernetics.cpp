2e02201a5a52a9e7c296cacd727e67b77a54e08b
kuberlog <collinalexbell@gmail.com>
2022-04-03 09:44:18 +0000

    "describe the product"

--- Tree_Cybernetics.cpp
+++ Tree_Cybernetics.cpp
@@ -10,6 +10,7 @@ struct Target_Revenue {
 
 class Product {
   string name;
+  string description;
   int unit_revenue;
   int unit_cost;
   int unit_profit;
@@ -19,6 +20,12 @@ class Product {
     unit_revenue = revenue;
     unit_profit = profit;
     unit_cost =  revenue-profit;
+  }
+  void describe() {
+    cout << name << " :\n" << description << endl;
+  }
+  void add_description(string description){
+    this->description = description;
   }
   int sales_to_target(Target_Revenue target) {
     int units_sold = target.dollars/unit_profit;
@@ -40,13 +47,15 @@ class Tree_Cybernetics {
         );
   }
 
-  void set_init_product(string name, int revenue, int profit) { 
+  void set_init_product(string name, string description, int revenue, int profit) { 
     init_product = new Product(name, revenue, profit);
+    init_product->add_description(description);
   }
   ~Tree_Cybernetics() {
     delete init_capital;
     delete init_product;
   }
+  
   void where_are_we_going() {
     cout << init_capital->dollars << "$ by " << init_capital->earn_by << endl;
   }
@@ -54,12 +63,16 @@ class Tree_Cybernetics {
     string choice;
     cout << "1) Where are we going?" << endl;
     cout << "2) Sales to target?" << endl;
+    cout << "3) Describe the product" << endl;
     cin >> choice;
     if(choice == "1"){
       where_are_we_going();
     }
     if(choice == "2"){
       cout << init_product->sales_to_target(*init_capital) << endl;
+    }
+    if(choice == "3"){
+      init_product->describe();
     }
   }
 };
@@ -76,6 +89,7 @@ int main() {
 
   kuberlogs_company.set_init_product(
       "cybernetics factory",
+      "an rpg about a young entrepreneur wishing to build and run many cybernetics factories at a profit to fund their industrialist dreams",
       40,
       40
       );
