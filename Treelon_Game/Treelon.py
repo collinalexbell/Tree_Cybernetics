8f3957386bfb611dad261f04508c386b3c5e207b
kuberlog <collinalexbell@gmail.com>
2022-04-10 22:37:44 +0000

    created game loop, runs way to fast, time for a clock

--- Treelon_Game/Treelon.py
+++ Treelon_Game/Treelon.py
@@ -16,17 +16,20 @@ class Treelon:
         self.primary_aim = primary_aim
         pygame.init()
         self.screen = pygame.display.set_mode([320, 240])
-        self.screen.fill((0,0,0))
+        self.screen.fill((254,254,254))
+        self.Zeus = pygame.image.load("Zeus.png")
+        self.Collin = pygame.image.load("collin.png")
+        self.zeus_pos = self.Zeus.get_rect()
+        self.zeus_pos = self.zeus_pos.move(20,20)
         self.display_zeus()
-        pygame.display.flip()
 
     def display_zeus(self):
-        Zeus = pygame.image.load("Zeus.png")
-        Collin = pygame.image.load("collin.png")
-        collin_position = Collin.get_rect()
-        moved_position = collin_position.move(150,50)
-        self.screen.blit(Zeus, Zeus.get_rect())
-        self.screen.blit(Collin, moved_position)
+        self.screen.fill((254,254,254))
+        collin_position = self.Collin.get_rect()
+        moved_position = self.Collin.get_rect().move(150,50)
+        self.screen.blit(self.Zeus, self.zeus_pos)
+        self.screen.blit(self.Collin, moved_position)
+        pygame.display.flip()
 
     def report(self):
         print("sdl version:")
@@ -36,6 +39,8 @@ class Treelon:
 def kuberlog():
     treelon =  Treelon("\n\nkuberlog", "to become the best software engineer in the world and earn 10e9$ profit by building ethical, benificial, and purposeful cybernetics")
 
+    while(True):
+        treelon.display_zeus()
 
     print("""
                        __
