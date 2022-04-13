4fe2e8a6d2c79a3c0526e720390d937b28726695
kuberlog <collinalexbell@gmail.com>
2022-04-13 00:52:20 +0000

    Zeus moves

--- Treelon_Game/Treelon.py
+++ Treelon_Game/Treelon.py
@@ -5,10 +5,22 @@
 import pygame
 import pygame.image
 import pygame.display
+import time
 
 class Zeus:
     def __init__(self):
         self.sprite = pygame.image.load("Zeus.png")
+        self.pos = (20,20)
+
+    def tick(self):
+        if(self.pos[0] < 100):
+            self.pos = tuple(map(sum, zip(self.pos, (1,1))))
+            time.sleep(0.04)
+
+    def get_pos(self):
+        return self.pos
+
+
 
 class Treelon:
     #         is an adventure game about becoming a world famous billionaire cyberneticist
@@ -23,16 +35,16 @@ class Treelon:
         self.screen.fill((254,254,254))
         self.Collin = pygame.image.load("collin.png")
         self.Zeus = Zeus()
-        self.zeus_pos = self.Zeus.sprite.get_rect()
-        self.zeus_pos = self.zeus_pos.move(20,20)
         self.display_zeus()
 
     def display_zeus(self):
+        self.Zeus.tick()
         self.screen.fill((254,254,254))
         collin_position = self.Collin.get_rect()
         moved_position = self.Collin.get_rect().move(150,50)
-        self.screen.blit(self.Zeus.sprite, self.zeus_pos)
+        print(self.Zeus.get_pos())
         self.screen.blit(self.Collin, moved_position)
+        self.screen.blit(self.Zeus.sprite, self.Zeus.get_pos())
         pygame.display.flip()
 
     def report(self):
