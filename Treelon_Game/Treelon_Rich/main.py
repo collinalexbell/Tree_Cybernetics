dcd8bc4b5e97e185eb907d892b572274a1f29640
kuberlog <collinalexbell@gmail.com>
2022-05-02 01:16:51 +0000

    add a console.rule(...)

--- Treelon_Game/Treelon_Rich/main.py
+++ Treelon_Game/Treelon_Rich/main.py
@@ -4,4 +4,5 @@ from rich.console import Console
 print("Hello, [bold magenta]World[/bold magenta]!", ":vampire:", locals())
 
 console = Console()
+console.rule("[bold red]Treelon")
 console.print([1,2,3])
