13a4fcc35de5a38bde38ba9093005fe2c4ff0ad4
kuberlog <collinalexbell@gmail.com>
2022-05-02 03:10:18 +0000

    deleted Treelon.lisp appearntly

--- Treelon_Game/Treelon.lisp
+++
@@ -1,8 +0,0 @@
-
-(ql:quickload :cl-opengl)
-(defpackage :treelon (:use :cl))
-
-(use-package :treelon)
-
-(defun hunt-for-opengl (name)
-  (format t "Velcomen, senor ~s~%Ve shall hunt for ze opengl example" name))
