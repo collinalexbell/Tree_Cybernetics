108eb2bb45895f60f80effd4baddb441aecda42a
kuberlog <collinalexbell@gmail.com>
2022-05-02 03:15:57 +0000

    builder mode is out of order

--- Treelon_Game/BuilderMode.py
+++ Treelon_Game/BuilderMode.py
@@ -2,4 +2,4 @@
 ### Module: BuilderMode, a whole new screen for designing and building cybernetics
 
 def init_builder_mode():
-    return "init builder mode"
+    print("sorry, builder mode is out of order")
