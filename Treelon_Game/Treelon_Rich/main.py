e797633993c9ddb2a575598f6e390f2bba500270
kuberlog <collinalexbell@gmail.com>
2022-05-02 02:29:39 +0000

    boot into main.py

--- Treelon_Game/Treelon_Rich/main.py
+++ Treelon_Game/Treelon_Rich/main.py
@@ -4,4 +4,24 @@ from rich.console import Console
 console = Console()
 console.rule("[bold red]Treelon")
 console.print("[bold magenta]Treelon[/bold magenta]!", locals())
-console.print([1,2,3])
+console.print([1,2,3], "[bold red]GO![/bold red]")
+
+class I:
+    def __init__(self, name):
+        self.name = name
+
+class U:
+    def __init__(self):
+        self.things = []
+    def add(self, things):
+        self.things.extend(things)
+
+
+U_ = U()
+
+collin = I("collin")
+zeus = I("zeus")
+onix = I("onlix")
+U_.add([collin, zeus, onix])
+
+console.print("[bold magenta]Holons:[/bold magenta]", U_.things)
