ae8335f2dab36fd9f320fe1d45d668290734aa29
Collin Bell <collinalexbell@gmail.com>
2022-05-14 15:07:26 -0400

    os.environ.get

--- Treelon_Game/treegame.py
+++ Treelon_Game/treegame.py
@@ -1,1 +1,17 @@
 
+import os
+
+config = os.environ.get('TREEGAME')
+
+if config == "":
+  print("Treegame")
+
+if config == "test":
+  print("Treegame Test")
+ 
+if config == "pygame":
+  print("Treegame Pygame")
+ 
+if config == "term":
+  print("Treegame Term")
+
