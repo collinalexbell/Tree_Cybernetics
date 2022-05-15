d8c4d65401395302440816291292b23ed4bcab04
kuberlog <collinalexbell@gmail.com>
2022-05-15 04:57:15 +0000

    added Vector

--- Treelon_Game/treegame.py
+++ Treelon_Game/treegame.py
@@ -5,15 +5,23 @@ config = os.environ.get('TREEGAME')
 
 print("init treegame")
 if config == None:
-  print("Treegame")
+    print("Treegame")
 
 if config == "test":
-  print("Treegame Test")
- 
-if config == "pygame":
-  print("Treegame Pygame")
-  from pygame import *
- 
-if config == "term":
-  print("Treegame Term")
+    print("Treegame Test")
 
+if config == "pygame":
+    print("Treegame Pygame")
+    from pygame import *
+
+if config == "term":
+    print("Treegame Term")
+
+
+class Vector:
+    """Vectors are absolutely essential to this game engine.
+    Intuitive understanding of them is a prerequisite:
+         to comfortable with this game engine"""
+
+    def __init__(self):
+        pass
