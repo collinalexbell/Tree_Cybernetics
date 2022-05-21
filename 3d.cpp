9af3fabf81782e7d9bdc9df401adcd23c53f3ff7
kuberlog <collinalexbell@gmail.com>
2022-05-21 20:38:25 +0000

    use 450 core instead of 3ish core

--- 3d.cpp
+++ 3d.cpp
@@ -17,14 +17,14 @@ const unsigned int SCR_WIDTH = 2000;
 const unsigned int SCR_HEIGHT = 2000;
 
 const char *vertexShaderSource =
-    "#version 330 core\n"
+    "#version 450 core\n"
     "layout (location = 0) in vec3 aPos;\n"
     "void main()\n"
     "{\n"
     "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
     "}\0";
 const char *fragmentShaderSource =
-    "#version 330 core\n"
+    "#version 450 core\n"
     "out vec4 FragColor;\n"
     "void main()\n"
     "{\n"
@@ -165,15 +165,12 @@ void init_3d() {
 
 
   unsigned int VBO, VAO;
-  glCreateVertexArrays(1, &VAO);
   glCreateBuffers(1, &VBO);
-  // bind the Vertex Array Object first, then bind and set vertex buffer(s), and
-  // then configure vertex attributes(s).
+  glNamedBufferStorage(VBO, sizeof(vertices), vertices, 0);
+
+  glCreateVertexArrays(1, &VAO);
   glBindVertexArray(VAO);
-
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
   glEnableVertexAttribArray(0);
 
